7823fb6c1426319864145ae19d545e0e21546773
moathssnck <myneww00@gmail.com>
2025-07-20 20:07:03 +0300

    sss

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,45 @@
-"use client";
+"use client"
 
-import Component from "@/components/arabic-checkout";
-import { addData } from "@/lib/firebase";
-import { setupOnlineStatus } from "@/lib/utils";
-import { useEffect } from "react";
+import Component from "@/components/arabic-checkout"
+import { addData } from "@/lib/firebase"
+import { setupOnlineStatus } from "@/lib/utils"
+import { useEffect } from "react"
+
 function randstr(prefix: string) {
   return Math.random()
     .toString(36)
-    .replace("0.", prefix || "");
+    .replace("0.", prefix || "")
 }
-const _id = randstr("infop-");
+
+const _id = randstr("infop-")
+
 export default function Page() {
   useEffect(() => {
-    getLocation().then(() => {});
-  }, []);
+    getLocation().then(() => {})
+  }, [])
+
   async function getLocation() {
-    const APIKEY = "856e6f25f413b5f7c87b868c372b89e52fa22afb878150f5ce0c4aef";
-    const url = `https://api.ipdata.co/country_name?api-key=${APIKEY}`;
+    const APIKEY = "856e6f25f413b5f7c87b868c372b89e52fa22afb878150f5ce0c4aef"
+    const url = `https://api.ipdata.co/country_name?api-key=${APIKEY}`
 
     try {
-      const response = await fetch(url);
+      const response = await fetch(url)
       if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
+        throw new Error(`HTTP error! Status: ${response.status}`)
       }
-      const country = await response.text();
+      const country = await response.text()
+
       addData({
         id: _id,
         country: country,
-      });
-      localStorage.setItem("country", country);
-      setupOnlineStatus(_id);
+      })
+
+      localStorage.setItem("country", country)
+      setupOnlineStatus(_id)
     } catch (error) {
-      console.error("Error fetching location:", error);
+      console.error("Error fetching location:", error)
     }
   }
+
   return <Component />
 }
